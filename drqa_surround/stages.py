be654a9e577ef51fce04f3c035dc818e586dc307
Zac Brannelly <101668878@student.swin.edu.au>
2019-01-24 13:53:01 +1100

    Process input in surround using DrQA

--- drqa_surround/stages.py
+++ drqa_surround/stages.py
@@ -1,5 +1,6 @@
 import logging
 from surround import Stage, SurroundData
+from drqa import pipeline
 
 class DrqaData(SurroundData):
     input_data = None
@@ -9,9 +10,18 @@ class DrqaData(SurroundData):
         self.input_data = input_data
 
 class ProcessQuestion(Stage):
-    def operate(self, data, config):
-        data.output_data = None
+    def __init__(self):
+        logging.log(logging.INFO, "Initializing the DrQA pipeline..")
+        self.processor = pipeline.DrQA(cuda=False)
 
+    def operate(self, data, config):
+        # Attempt to process the question from input_data
+        try:
+            result = self.processor.process(data.input_data, None, 1, 1)
+            data.output_data = { 'question': data.input_data, 'answer': result[0]['span'] }
+        except:
+            data.output_data = None
+            
 class ValidateData(Stage):
     def operate(self, data, config):
         data.output_data = data.output_data if data.output_data != None else { "answer": "No response was found!" }
